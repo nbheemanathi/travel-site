0bc13dbab42b47069ed83c070b8fbdac22489ca6
nbheema <bheemanathi.nihar@gmail.com>
2018-05-21 23:35:01 -0700

    BEM

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 gulp.task('default', function(){
     console.log("hello");
@@ -12,7 +17,9 @@ gulp.task('html', function()
 
 gulp.task('styles', function()
 {
-  return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch', function(){
     watch('./app/index.html', function(){
