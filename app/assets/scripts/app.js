78def64add8e34e1701d2bd25d04edc74207c621
nbheema <bheemanathi.nihar@gmail.com>
2018-06-18 23:59:40 -0700

    overlay features

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var menu = new MobileMenu();
 // var reveal = new RevealOnScroll();
@@ -9,3 +10,4 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "65%");
 
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
